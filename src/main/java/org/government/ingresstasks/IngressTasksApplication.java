882b149328f642b153ae570709abfee4c45a79b1
hasan.huseynov <Bgnnysm200116@>
2024-07-17 05:48:50 -0700

    Adding base project

---
+++ src/main/java/org/government/ingresstasks/IngressTasksApplication.java
@@ -0,0 +1,13 @@
+package org.government.ingresstasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngressTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/government/ingresstasks/IngressTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.government.ingresstasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngressTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
